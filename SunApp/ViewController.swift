8620d9c86fc677b5f4de35849a419b6b383a2e01
sun-23 <twinsamson@gmail.com>
2019-04-20 19:09:21 +0700

    Initial Commit

---
+++ SunApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SunApp
+//
+//  Created by sun on 20/4/2562 BE.
+//  Copyright © 2562 sun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
