b48b3c20e1b4a7d11766418395f180483dffdcfc
sun-23 <twinsamson@gmail.com>
2019-04-21 15:48:11 +0700

    walloper ok

--- SunApp/ViewController.swift
+++ SunApp/ViewController.swift
@@ -9,11 +9,63 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var userTextField: UITextField!
+    
+    
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    
+    @IBAction func loginButton(_ sender: UIButton) {
+        
+        print("You Click Login")
+        
+        
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+    @IBAction func registerButton(_ sender: UIButton) {
+        
+        print("You Click Register")
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
 
 }
