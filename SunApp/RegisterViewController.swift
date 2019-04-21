a7caab567e71c0c4e622b6f9acedf1276e5f6227
sun-23 <twinsamson@gmail.com>
2019-04-21 17:22:01 +0700

    BackButtonOK

--- SunApp/RegisterViewController.swift
+++ SunApp/RegisterViewController.swift
@@ -9,6 +9,15 @@
 import UIKit
 
 class RegisterViewController: UIViewController {
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,15 +25,22 @@ class RegisterViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func UpLoadButton(_ sender: UIBarButtonItem) {
+        
+        print("You Click UpLoad")
+        
+      performSegue(withIdentifier: "BackToAuthen", sender: nil)
+        
+        
     }
-    */
-
+    
+    @IBAction func BackToRegister(_ sender: UIBarButtonItem) {
+        
+        print("You Click BackToRegister")
+        
+        performSegue(withIdentifier: "BackToAuthen", sender: nil)
+        
+    }
+    
 }
--- SunApp/ViewController.swift
+++ SunApp/ViewController.swift
@@ -29,7 +29,7 @@ class ViewController: UIViewController {
         
         print("You Click Login")
         
-        
+
         
         
         
